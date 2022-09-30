f4306fc0410d75f7f48220e96cc9c2dc402ec355
Kelley Erickson <kerickson@mobot.org>
2022-09-30 17:03:03 -0500

    Update 02_Model_5_Joint_Hmsc_Computer1.R

--- code/02_Model_5_Joint_Hmsc_Computer1.R
+++ code/02_Model_5_Joint_Hmsc_Computer1.R
@@ -1,4 +1,4 @@
-# Run Hmsc joint on computer 2
+# Run Hmsc joint on computer 1
 if(Sys.info()['sysname'] == "Darwin") {
 path <- "/Users/curculion/Documents/GitHub"
 }
@@ -9,11 +9,11 @@ path <- "C:/Users/kerickson/Documents/GitHub"
 
 source(paste0(path, "/SDMs_for_rare_species_modeling/code/00b_Constants.R"))
 
-repStart <- 1
-repEnd <- 25
+repStart <- 76
+repEnd <- 100
 
 session <- sessionInfo()
-save(session, file=paste0(path, "/SDMs_for_rare_species_modeling/data/models/Hmsc_joint/sessionInfo_computer2.RData"))
+save(session, file=paste0(path2, "/models/Hmsc_joint/sessionInfo_computer1.RData"))
 
 
 

b61d1df50db4ae4d38bd3957a2469cdb0d91b904
Kelley Erickson <kerickson22@gmail.com>
2022-10-18 12:19:53 -0500

    update computer schedule

--- code/02_Model_5_Joint_Hmsc_Computer2.R
+++ code/02_Model_5_Joint_Hmsc_Computer2.R
@@ -1,17 +1,21 @@
 # Run Hmsc joint on computer 2
 if(Sys.info()['sysname'] == "Darwin") {
 path <- "/Users/curculion/Documents/GitHub"
-path2 <- path
 }
 
 if(Sys.info()['sysname'] == "Windows") {
 path <- "C:/Users/kerickson/Documents/GitHub"
-path2 <- "H:/Global Change Program/Research/ENMs - Modeling Methods for Rare Species (Kelley Erickson)/rare_species/data"
 }
 
 source(paste0(path, "/SDMs_for_rare_species_modeling/code/00b_Constants.R"))
 
-reps <- sample(1:100)
+comp1 <- 1:25
+comp2 <- 26:50
+comp3 <- 51:75
+comp4 <- 76:100
+
+reps <- c(comp2, comp3[25:1], comp4[25:1],
+          comp1[25:1])
 
 session <- sessionInfo()
 save(session, file=paste0(path2, "/models/Hmsc_joint/sessionInfo_computer2.RData"))
@@ -19,7 +23,14 @@ save(session, file=paste0(path2, "/models/Hmsc_joint/sessionInfo_computer2.RData
 
 
 source(paste0(path, "/SDMS_for_rare_species_modeling/code/02_Model_5_Joint_Hmsc.R"))
+
 if(!file.exists(paste0(path2, "/models/",
-                       modelType[1], "/results.RData" ))) {
-  source(paste0(path, "/SDMS_for_rare_species_modeling/code/03_Collate_Results.R"))
+                       modelType[1], "/results_start.RData" ))) {
+  k <- "computer_2"
+  save(k, file=paste0(path2, "/models/",
+                       modelType[1], "/results_start.RData"))
+  myfile <- paste0(path2, "/models/",
+                   modelType[1], "/results_start.RData" ) %>%
+    drive_upload(paste0("status_updates_for_Hmsc_joint/", k, ".RData"))
+  source(paste0(path, "/SDMS_for_rare_species_modeling/code/03_Collate_Results_joint_Hmsc.R"))
 }
--- code/02_Model_5_Joint_Hmsc_Computer3.R
+++ code/02_Model_5_Joint_Hmsc_Computer3.R
@@ -1,4 +1,4 @@
-# Run Hmsc joint on computer 2
+# Run Hmsc joint on computer 3
 if(Sys.info()['sysname'] == "Darwin") {
 path <- "/Users/curculion/Documents/GitHub"
 }
@@ -9,15 +9,28 @@ path <- "C:/Users/kerickson/Documents/GitHub"
 
 source(paste0(path, "/SDMs_for_rare_species_modeling/code/00b_Constants.R"))
 
-reps <- sample(1:100)
+comp1 <- 1:25
+comp2 <- 26:50
+comp3 <- 51:75
+comp4 <- 76:100
+
+reps <- c(comp3, comp4[25:1], comp2[25:1],
+          comp1[25:1])
 
 session <- sessionInfo()
-save(session, file=paste0(path, "/SDMs_for_rare_species_modeling/data/models/Hmsc_joint/sessionInfo_computer2.RData"))
+save(session, file=paste0(path2, "/models/Hmsc_joint/sessionInfo_computer3.RData"))
 
 
 
 source(paste0(path, "/SDMS_for_rare_species_modeling/code/02_Model_5_Joint_Hmsc.R"))
+
 if(!file.exists(paste0(path2, "/models/",
-                       modelType[1], "/results.RData" ))) {
-  source(paste0(path, "/SDMS_for_rare_species_modeling/code/03_Collate_Results.R"))
+                       modelType[1], "/results_start.RData" ))) {
+  k <- "computer_3"
+  save(k, file=paste0(path2, "/models/",
+                       modelType[1], "/results_start.RData"))
+  myfile <- paste0(path2, "/models/",
+                   modelType[1], "/results_start.RData" ) %>%
+    drive_upload(paste0("status_updates_for_Hmsc_joint/", k, ".RData"))
+  source(paste0(path, "/SDMS_for_rare_species_modeling/code/03_Collate_Results_joint_Hmsc.R"))
 }
